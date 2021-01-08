df18a78c3e84104e4570e718437018883b2d4a95
GyebnarPeter <gyebnarpetter@gmail.com>
2021-01-08 16:23:13 +0100

    i have added the footer

--- libs/js/script.js
+++ libs/js/script.js
@@ -4,11 +4,9 @@ let navBtn = document.querySelector(".nav-btn");
 let navCloseBtn = document.querySelector(".nav-close-btn");
 
 navBtn.onclick = () => {
-    //console.log("klikk")
     nav.classList.add("nav-toggle");
 }
 
 navCloseBtn.onclick = () => {
-    //console.log("klikk")
     nav.classList.remove("nav-toggle");
 }
