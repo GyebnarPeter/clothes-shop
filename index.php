aea58c0dfc1b88296ef872eed99940c1cfd7424b
GyebnarPeter <gyebnarpetter@gmail.com>
2021-01-11 14:29:59 +0100

    new product part, in index site

--- index.php
+++ index.php
@@ -1,5 +1,116 @@
 <?php include 'includes/header.php' ?>
 
-<main></main>
+<div class="slider">
+    <img src="img/slider.jpg" alt="">
+</div>
+
+<div class="new-products">
+
+    <div class="new-products-head">
+        <h2>new design trends</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</p>
+    </div>
+
+    <div class="new-products-wrapper">
+
+        <div class="new-product">
+            <img src="img/new-product-1.jpg" alt="">
+
+            <div class="product-name-wrapper">
+                <div class="product-name">
+                    <h4>sunglasses</h4>
+                    <span></span>
+                    <p>3 items</p>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="new-product">
+            <img src="img/new-product-2.jpg" alt="">
+
+            <div class="product-name-wrapper">
+                <div class="product-name">
+                    <h4>sunglasses</h4>
+                    <span></span>
+                    <p>3 items</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="new-product">
+            <img src="img/new-product-3.jpg" alt="">
+
+            <div class="product-name-wrapper">
+                <div class="product-name">
+                    <h4>sunglasses</h4>
+                    <span></span>
+                    <p>3 items</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="new-product">
+            <img src="img/new-product-4.jpg" alt="">
+
+            <div class="product-name-wrapper">
+                <div class="product-name">
+                    <h4>sunglasses</h4>
+                    <span></span>
+                    <p>3 items</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="new-product">
+            <img src="img/new-product-5.jpg" alt="">
+
+            <div class="product-name-wrapper">
+                <div class="product-name">
+                    <h4>sunglasses</h4>
+                    <span></span>
+                    <p>3 items</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="new-product">
+            <img src="img/new-product-6.jpg" alt="">
+
+            <div class="product-name-wrapper">
+                <div class="product-name">
+                    <h4>sunglasses</h4>
+                    <span></span>
+                    <p>3 items</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="new-product">
+            <img src="img/new-product-7.jpg" alt="">
+
+            <div class="product-name-wrapper">
+                <div class="product-name">
+                    <h4>sunglasses</h4>
+                    <span></span>
+                    <p>3 items</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="new-product">
+            <img src="img/new-product-8.jpg" alt="">
+
+            <div class="product-name-wrapper">
+                <div class="product-name">
+                    <h4>sunglasses</h4>
+                    <span></span>
+                    <p>3 items</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
 
 <?php include 'includes/footer.php' ?>
